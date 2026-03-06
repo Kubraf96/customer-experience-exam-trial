a60f1c669b530e691abf34266653fadf565b8745
millehlauridsen-ops <millehlauridsen@gmail.com>
2026-03-06 13:16:05 +0100

    struktureret svg'er i hvid og sort samt lavet layout i navigationen.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,18 +4,31 @@ import "../css/almNav.css";
 export default function Navbar() {
   return (
     <nav>
-      <NavLink to="/">
-        <img src="./images/components/logo.svg" alt="Logo" />
-      </NavLink>
-      <img src="./images/components/navline.svg" alt="line" />
-      <NavLink to="/about">About</NavLink>
-      <img src="./images/components/navline.svg" alt="line" />
-      <NavLink to="/contact">Contact</NavLink>
-      <img src="./images/components/navline.svg" alt="line" />
-      <NavLink to="/product">Product</NavLink>
-      <img src="./images/components/navline.svg" alt="line" />
-      <NavLink to="/sale">Sale</NavLink>
+      <div className="nav-links">
+        <NavLink to="/">
+          <img src="./images/components/logo.svg" alt="Logo" />
+        </NavLink>
+        <img src="./images/components/whitenav/navline.svg" alt="line" />
+        <NavLink to="/about">About</NavLink>
+        <img src="./images/components/whitenav/navline.svg" alt="line" />
+        <NavLink to="/contact">Contact</NavLink>
+        <img src="./images/components/whitenav/navline.svg" alt="line" />
+        <NavLink to="/product">Product</NavLink>
+        <img src="./images/components/whitenav/navline.svg" alt="line" />
+        <NavLink to="/sale">Sale</NavLink>
+      </div>
+      <div>
+        <img src="./images/components/whitenav/search.svg" alt="search" />
+        <NavLink to="/pinned">
+          <img src="./images/components/whitenav/pin.svg" alt="pinned pieces" />
+        </NavLink>
+        <NavLink to="/notfoundpage">
+          <img src="./images/components/whitenav/user.svg" alt="profile" />
+        </NavLink>
+        <NavLink to="/cart">
+          <img src="./images/components/whitenav/cart.svg" alt="Cart" />
+        </NavLink>
+      </div>
     </nav>
-    <div></div>
   );
 }
