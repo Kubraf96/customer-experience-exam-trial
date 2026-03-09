866aca50814703e35162952cd8f4a8de1ba98843
millehlauridsen-ops <millehlauridsen@gmail.com>
2026-03-09 13:09:43 +0100

    rettelser i footer og nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,27 +6,30 @@ export default function Navbar() {
     <nav>
       <div className="nav-links">
         <NavLink to="/">
-          <img src="./images/components/whitenav/logo.svg" alt="Logo" />
+          <img src="./src/assets/components/whitenav/logo.svg" alt="Logo" />
         </NavLink>
-        <img src="./images/components/whitenav/navline.svg" alt="line" />
+        <img src="./src/assets/components/whitenav/navline.svg" alt="line" />
         <NavLink to="/about">About</NavLink>
-        <img src="./images/components/whitenav/navline.svg" alt="line" />
+        <img src="./src/assets/components/whitenav/navline.svg" alt="line" />
         <NavLink to="/contact">Contact</NavLink>
-        <img src="./images/components/whitenav/navline.svg" alt="line" />
+        <img src="./src/assets/components/whitenav/navline.svg" alt="line" />
         <NavLink to="/product">Product</NavLink>
-        <img src="./images/components/whitenav/navline.svg" alt="line" />
+        <img src="./src/assets/components/whitenav/navline.svg" alt="line" />
         <NavLink to="/sale">Sale</NavLink>
       </div>
       <div className="nav-icons">
-        <img src="./images/components/whitenav/search.svg" alt="search" />
+        <img src="./src/assets/components/whitenav/search.svg" alt="search" />
         <NavLink to="/pinned">
-          <img src="./images/components/whitenav/pin.svg" alt="pinned pieces" />
+          <img
+            src="./src/assets/components/whitenav/pin.svg"
+            alt="pinned pieces"
+          />
         </NavLink>
         <NavLink to="/notfoundpage">
-          <img src="./images/components/whitenav/user.svg" alt="profile" />
+          <img src="./src/assets/components/whitenav/user.svg" alt="profile" />
         </NavLink>
         <NavLink to="/cart">
-          <img src="./images/components/whitenav/cart.svg" alt="Cart" />
+          <img src="./src/assets/components/whitenav/cart.svg" alt="Cart" />
         </NavLink>
       </div>
     </nav>
